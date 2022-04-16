24026267c67e34ba7f440ac4984421717978aa95
Jonathan Kong Boon Lieng <jonathankong96@hotmail.com>
2022-04-16 15:15:37 +0800

    Update binarySearch.java

--- DSA/src/binarySearch.java
+++ DSA/src/binarySearch.java
@@ -2,8 +2,21 @@
 public class binarySearch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		
+		int left = 0;
+		int right = nums.length - 1;
+		
+		while(left <= right){
+			int middle = (left+right)/2;
+			if(nums[middle] == target){
+				return middle;
+			}
+			if(target > nums[middle]{
+				left = middle + 1;
+			}
+			else{
+				right = middle - 1;
+			}
+		}
+	return -1;
 }
